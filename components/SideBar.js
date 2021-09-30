e221da7d86434e444eb31e0b61c34ff3b035fa6a
‘julian-leahy’ <julian.leahy.dev@gmail.com>
2021-09-30 15:35:45 +1300

    text

--- components/SideBar.js
+++ components/SideBar.js
@@ -10,17 +10,6 @@ export default function SideBar() {
         voluptates et at perspiciatis, porro vel incidunt consequuntur tenetur,
         corporis sapiente officia omnis ut quod magni beatae amet obcaecati.
         Laborum perferendis, maxime possimus, illum culpa neque dolorem porro
-        illo incidunt eius dolore rerum eos. Id sed nostrum tenetur dolores
-        minus enim asperiores. Accusantium error molestias itaque illo ut iste
-        voluptatibus minima exercitationem, accusamus harum et rem. Doloremque
-        earum impedit architecto eum, quod eaque voluptatem quos, cupiditate
-        minus nisi minima labore itaque asperiores corporis laboriosam
-        recusandae. Id ad illum, modi dolorem doloremque labore totam nihil nemo
-        veritatis corporis? Ipsam, laboriosam rem ex corrupti culpa possimus
-        mollitia quis consequuntur ducimus doloremque tenetur? Fuga
-        reprehenderit quam voluptatem tempore provident. Ad corrupti suscipit
-        aliquam? Cum ex nulla dignissimos! Voluptatum, eum cupiditate alias id
-        molestias reprehenderit magnam modi aut similique minima eius ratione,
       </div>
       <style jsx>{`
         .sidebar {
